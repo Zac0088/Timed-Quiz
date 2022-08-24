3077db00c48f3c7578e80f3007b5e3db79bc101b
Zac Smart <zac_chaz@outlook.com>
2022-08-24 11:54:11 +1000

    added quiz questions

---
+++ script.js
@@ -0,0 +1,35 @@
+var startButton = document.getElementById("beginQuiz");
+var submit = document.getElementById("submit");
+var timer = document.getElementById("timer");
+var clear = document.getElementById("clear");
+var goBack = document.getElementById("goBack");
+
+var questions = [
+    {
+        title: "commonly used data types DO NOT include?",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Arrays in javascript can be used to store what?",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to debugger is? ",
+        choices: ["javascript", "terminal/bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within?",
+        choices: ["quotes", "curly brackets", "paraentheses", "square brackets"],
+        answer: "curly brackets"
+    },
+    {
+        title: "string values must be enclosed within____ when being assigned to variables",
+        choices: ["commas", "curley brackets", "quotes", "perentheses"],
+        answer: "commas"
+    },
+
+]
